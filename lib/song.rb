1799d1a132edbf797d6062641f3e7f1036bf87c1
Chris Kerbo <chris.kerbo@outlook.com>
2019-10-02 16:01:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,12 +14,7 @@ class Song
   end
 
   def artist_name=(name)
-    if (self.artist.nil?)
-     Artist.find_or_create_by_name(name)
-    else
-     self.artist.name = name
-    end
-    self.artist.add_song(name)
+    Artist.find_or_create_by_name(name)
   end
 
   def self.new_by_filename(filename)
