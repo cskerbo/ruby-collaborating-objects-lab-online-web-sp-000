e59c05505fbab914cd5bd6e0c544f2a777d391a1
Chris Kerbo <chris.kerbo@outlook.com>
2019-10-02 16:07:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,7 +23,7 @@ class Song
     song_name = parts[1].gsub(".mp3", "")
     #Pending artist_name=() completion
     song = self.new(song_name)
-    song.artist.name(artist_name)
+    song.artist_name = artist_name
     #Pending artist_name=() completion
   end
 
